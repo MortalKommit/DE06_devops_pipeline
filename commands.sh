758397fe1850e0e55d9750da0b497e1691d3f07c
MortalKommit <kedzko@gmail.com>
2022-09-05 07:30:12 +0530

    Added if condition to check if installation exists

--- commands.sh
+++ commands.sh
@@ -1,5 +1,10 @@
 # Download miniconda for python 3.7
-wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.9.2-Linux-x86_64.sh && sh Miniconda3-py37_4.9.2-Linux-x86_64.sh -u -b -p
+if  [[ -n $(which conda)]]
+then 
+    echo "Conda not found"
+else echo "Conda found"
+fi
+#wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.9.2-Linux-x86_64.sh && sh Miniconda3-py37_4.9.2-Linux-x86_64.sh -u -b -p
 
 # Add to path
 export PATH=~/miniconda3/bin:$PATH
