4c313c6a78312c3bb2b20659e2c8835b412fdb7f
MortalKommit <kedzko@gmail.com>
2022-09-05 07:23:18 +0530

    Removed clone step since it is already done

--- commands.sh
+++ commands.sh
@@ -1,7 +1,10 @@
 # Download miniconda for python 3.7
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.9.2-Linux-x86_64.sh && sh Miniconda3-py37_4.9.2-Linux-x86_64.sh -u -b -p
 
+# Add to path
 export PATH=~/miniconda3/bin:$PATH
 
+# Alias command
 alias python3=python3.7
-az webapp up -n flask-ml-service206174
+
+#az webapp up -n flask-ml-service206174
