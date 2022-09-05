72dd373cb6819a55aff62775ea22bc5242160c80
MortalKommit <kedzko@gmail.com>
2022-09-05 07:20:45 +0530

    Updated commands to install python3.7 and clone repo

--- commands.sh
+++ commands.sh
@@ -1,1 +1,7 @@
+# Download miniconda for python 3.7
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.9.2-Linux-x86_64.sh && sh Miniconda3-py37_4.9.2-Linux-x86_64.sh -u -b -p
+
+export PATH=~/miniconda3/bin:$PATH
+
+alias python3=python3.7
 az webapp up -n flask-ml-service206174
