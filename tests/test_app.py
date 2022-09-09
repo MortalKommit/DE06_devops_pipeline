dcb423b2e2940c2b36507dcc03778e87c8cef9a5
MortalKommit <kedzko@gmail.com>
2022-09-09 12:25:26 +0530

    Add post request test

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,22 +2,25 @@ from urllib import request
 import pytest
 import json
 import os
-import requests
 from app import app
+
 
 @pytest.fixture()
 def client():
-    with open("sample_request.json") as f:
-        app.config.from_pyfile('flask_test.cfg', silent=True)
-        client = app.test_client()
-        yield client
-        # request_data = json.load(f)
-        # r = requests.post(url="http://localhost:5000", data=request_data)        
-        # return r
+    app.config.from_pyfile('flask_test.cfg', silent=True)
+    client = app.test_client()
+    yield client
 
-def test_request_example(client):
+
+def test_homepage_request(client):
     response = client.get("/")
     assert b"<h3>Sklearn Prediction Home</h3>" in response.data
+
+
+def test_prediction_request(client):
+    with open("sample_request.json") as f:
+        request_data = json.load(f)
+        client.post("/predict", json=request_data)
 
 # def test_valid_json_payload(flask_response):
 #     assert type(flask_response) == dict
