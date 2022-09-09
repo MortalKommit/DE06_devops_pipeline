d8de47d33dfbf036dd8d8db7a1c165ab5995c75f
MortalKommit <kedzko@gmail.com>
2022-09-09 09:53:53 +0530

    Update locust tests

--- locustfile.py
+++ locustfile.py
@@ -1,26 +1,28 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 
-class HelloWorldUser(HttpUser):
+class FlaskMLAppUser(HttpUser):
+    # Wait between 5 to 10 seconds between task
+    wait_time = between(5, 10)
+
     @task
-    def hello_world(self):
-        self.client.get("/")
-        # self.client.post("/predict", json={"CHAS": {
-        #     "0": 0
-        # },
-        #     "RM": {
-        #     "0": 6.575
-        # },
-        #     "TAX": {
-        #     "0": 296.0
-        # },
-        #     "PTRATIO": {
-        #     "0": 15.3
-        # },
-        #     "B": {
-        #     "0": 396.9
-        # },
-        #     "LSTAT": {
-        #     "0": 4.98
-        # }
-        # })
+    def predict_price(self):
+        self.client.post("/predict", json={"CHAS": {
+            "0": 0
+        },
+            "RM": {
+            "0": 6.575
+        },
+            "TAX": {
+            "0": 296.0
+        },
+            "PTRATIO": {
+            "0": 15.3
+        },
+            "B": {
+            "0": 396.9
+        },
+            "LSTAT": {
+            "0": 4.98
+        }
+        })
